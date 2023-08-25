f2cebee7edcdb771477ae5dff0deec548de174f7
Jonathan Yeo <jonathanyji98_28@hotmail.com>
2023-08-25 17:05:20 +1000

    wip

--- content.js
+++ content.js
@@ -4,11 +4,59 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         try {
-            document.querySelector("input[data-automation-id='legalNameSection_firstName']").value = request.name,
-                document.querySelector("input[data-automation-id='legalNameSection_lastName']").value = request.lastName,
-                document.querySelector("input[data-automation-id='addressSection_city']").value = request.city,
-                document.querySelector("input[data-automation-id='addressSection_postalCode']").value = request.postCode,
-                document.querySelector("input[data-automation-id='phone-number']").value = request.phoneNumber
+
+            const map1 = new Map();
+            map1.set('legalNameSection_firstName', request.name);
+            map1.set('legalNameSection_lastName', request.lastName);
+            map1.set('addressSection_addressLine1', request.addressLine1);
+            map1.set('addressSection_city', request.city);
+            map1.set('addressSection_postalCode', request.postCode);
+            map1.set('phone-number', request.phoneNumber);
+
+            // Creating the first work experience object
+            const workExperience1 = new Map();
+            workExperience1.set("jobTitle", request.job1Title);
+            workExperience1.set("company", request.job1Company);
+            workExperience1.set("location", request.job1Location);
+            workExperience1.set("description", request.job1Description);
+
+            // Creating the second work experience object
+            const workExperience2 = new Map();
+            workExperience2.set("jobTitle", request.job2Title);
+            workExperience2.set("company", request.job2Company);
+            workExperience2.set("location", request.job2Location);
+            workExperience2.set("description", request.job2Description);
+
+            if (document.querySelector("div[data-automation-id='contactInformationPage']")){
+                for (let [key, val] of map1) {
+                    if (document.querySelector(`input[data-automation-id=${key}]`)){
+                        document.querySelector(`input[data-automation-id=${key}]`).value = val;
+                    }
+                }
+            }
+       
+            if (document.querySelector("div[data-automation-id='workExperienceSection']")){
+                for (let [key, val] of workExperience1) {
+                    if (key == 'description'){
+                        document.querySelector("div[data-automation-id='workExperience-1']").querySelector(`textarea[data-automation-id=${key}]`).value = val;
+                    } else {
+                        document.querySelector("div[data-automation-id='workExperience-1']").querySelector(`input[data-automation-id=${key}]`).value = val;
+                    }
+                }
+                for (let [key, val] of workExperience2) {
+                    if (key == 'description'){
+                        document.querySelector("div[data-automation-id='workExperience-2']").querySelector(`textarea[data-automation-id=${key}]`).value = val;
+                    } else {
+                        document.querySelector("div[data-automation-id='workExperience-2']").querySelector(`input[data-automation-id=${key}]`).value = val;
+                    }
+                }
+                
+            }
+
+            if (document.querySelector("div[data-automation-id='educationSection']")){
+                document.querySelector(`input[data-automation-id="school"]`).value = request.school;
+            }
+                
         } catch (error) {
             console.log(error)
             sendResponse({ status: "Exception occurred!" })
--- ext/script.js
+++ ext/script.js
@@ -5,9 +5,26 @@ document.getElementById("autofill").addEventListener("click", () => {
       {
         name: document.getElementById("firstName").value,
         lastName: document.getElementById("lastName").value,
+        addressLine1: document.getElementById("addressLine1").value,
         city: document.getElementById("city").value,
         postCode: document.getElementById("postCode").value,
         phoneNumber: document.getElementById("phoneNumber").value,
+
+        job1Title: document.getElementById("job1Title").value,
+        job1Company: document.getElementById("job1Company").value,
+        job1Location: document.getElementById("job1Location").value,
+        job1StartDate: document.getElementById("job1StartDate").value,
+        job1EndDate: document.getElementById("job1EndDate").value,
+        job1Description: document.getElementById("job1Description").value,
+
+        job2Title: document.getElementById("job2Title").value,
+        job2Company: document.getElementById("job2Company").value,
+        job2Location: document.getElementById("job2Location").value,
+        job2StartDate: document.getElementById("job2StartDate").value,
+        job2EndDate: document.getElementById("job2EndDate").value,
+        job2Description: document.getElementById("job2Description").value,
+
+        school: document.getElementById("school").value,
       },
       function (response) {
         console.log(response);
@@ -21,9 +38,26 @@ document.getElementById("save").addEventListener("click", () => {
     {
       name: document.getElementById("firstName").value,
       lastName: document.getElementById("lastName").value,
+      addressLine1: document.getElementById("addressLine1").value,
       city: document.getElementById("city").value,
       postCode: document.getElementById("postCode").value,
       phoneNumber: document.getElementById("phoneNumber").value,
+
+      job1Title: document.getElementById("job1Title").value,
+      job1Company: document.getElementById("job1Company").value,
+      job1Location: document.getElementById("job1Location").value,
+      job1StartDate: document.getElementById("job1StartDate").value,
+      job1EndDate: document.getElementById("job1EndDate").value,
+      job1Description: document.getElementById("job1Description").value,
+
+      job2Title: document.getElementById("job2Title").value,
+      job2Company: document.getElementById("job2Company").value,
+      job2Location: document.getElementById("job2Location").value,
+      job2StartDate: document.getElementById("job2StartDate").value,
+      job2EndDate: document.getElementById("job2EndDate").value,
+      job2Description: document.getElementById("job2Description").value,
+
+      school: document.getElementById("school").value,
     },
     function () {
       console.log("Saved");
@@ -33,13 +67,35 @@ document.getElementById("save").addEventListener("click", () => {
 
 document.getElementById("load").addEventListener("click", () => {
   chrome.storage.sync.get(
-    ["name", "lastName", "city", "postCode", "phoneNumber"],
+    [
+      "name", "lastName", "addressLine1", "city", "postCode", "phoneNumber",
+      "job1Title", "job1Company", "job1Location", "job1StartDate", "job1EndDate", "job1Description",
+      "job2Title", "job2Company", "job2Location", "job2StartDate", "job2EndDate", "job2Description",
+      "school",
+    ],
     function (result) {
       document.getElementById("firstName").value = result.name;
       document.getElementById("lastName").value = result.lastName;
+      document.getElementById("addressLine1").value = result.addressLine1;
       document.getElementById("city").value = result.city;
       document.getElementById("postCode").value = result.postCode;
       document.getElementById("phoneNumber").value = result.phoneNumber;
+
+      document.getElementById("job1Title").value = result.job1Title;
+      document.getElementById("job1Company").value = result.job1Company;
+      document.getElementById("job1Location").value = result.job1Location;
+      document.getElementById("job1StartDate").value = result.job1StartDate;
+      document.getElementById("job1EndDate").value = result.job1EndDate;
+      document.getElementById("job1Description").value = result.job1Description;
+
+      document.getElementById("job2Title").value = result.job2Title;
+      document.getElementById("job2Company").value = result.job2Company;
+      document.getElementById("job2Location").value = result.job2Location;
+      document.getElementById("job2StartDate").value = result.job2StartDate;
+      document.getElementById("job2EndDate").value = result.job2EndDate;
+      document.getElementById("job2Description").value = result.job2Description;
+
+      document.getElementById("school").value = result.school;
     }
   );
 });
